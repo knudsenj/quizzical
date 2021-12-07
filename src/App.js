5786e669b61ef445c53a6d8c5f92e2e36edc204d
Joel Knudsen <knudsen.joel@gmail.com>
2021-12-06 18:46:20 -0800

    Add start screen.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from "react"
+import Quiz from "./components/Quiz";
+
 function App() {
+  const [hasStarted, setHasStarted] = useState(false);
+
+  function startQuiz() {
+    setHasStarted(true);
+  }
+
   return (
-    <div className="App">
-      App Component
-    </div>
+    <main className="app-wrapper">
+      {
+        hasStarted ?
+          <Quiz /> :
+          <div className="start">
+            <h1 className="start--title">Quizzical</h1>
+            <h3 className="start--description">Challenge yourself with five question trivia quizzes!</h3>
+            <button className="start--button" onClick={startQuiz}>Start Quiz</button>
+          </div>
+      }
+    </main>
   );
 }
 
---
+++ src/components/Quiz.js
@@ -0,0 +1,9 @@
+function Quiz() {
+  // state: hasFinished, questions + answers, responses
+
+  return (
+    <h1>Quiz Component</h1>
+  );
+}
+
+export default Quiz;
