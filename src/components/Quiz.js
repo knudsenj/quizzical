2f6a97d428adfcf076f437cfa59c04d53f74907d
Joel Knudsen <knudsen.joel@gmail.com>
2021-12-06 20:56:01 -0800

    Display results and have option to play again.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,24 +1,28 @@
 import { useState, useEffect } from "react"
 import { nanoid } from "nanoid"
-import mockData from "../assets/mockData"
 import Question from "./Question";
 
 function Quiz() {
-  // state: hasFinished, questions + answers, responses
   const [questions, setQuestions] = useState([])
   const [hasFinished, setHasFinished] = useState(false);
 
   useEffect(() => {
-    setQuestions(mockData.results.map(item => ({
-      id: nanoid(),
-      question: item.question,
-      correctAnswer: item.correct_answer,
-      answers: [...item.incorrect_answers, item.correct_answer]
-        .sort(() => Math.random() - 0.5)
-        .map(answer => ({id: nanoid(), value: answer})),
-      selected: null,
-    })))
-  }, []);
+    if (!hasFinished) {
+      fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+        .then(res => res.json())
+        .then(data => (
+          setQuestions(data.results.map(item => ({
+            id: nanoid(),
+            question: item.question,
+            correctAnswer: item.correct_answer,
+            answers: [...item.incorrect_answers, item.correct_answer]
+              .sort(() => Math.random() - 0.5)
+              .map(answer => ({id: nanoid(), value: answer})),
+            selected: null,
+          })))
+        ))
+    }
+  }, [hasFinished])
 
   function handleAnswerSelected(questionId, answer) {
     setQuestions(prevQuestions => (
@@ -46,8 +50,15 @@ function Quiz() {
       ))}
 
       <div className="quiz--actions">
+        { hasFinished &&
+          <div className="quiz--results">
+            You scored&nbsp;
+            {questions.reduce((count, question) => count + (question.selected === question.correctAnswer), 0)}/{questions.length} 
+            &nbsp;correct answers
+          </div>
+        }
         <button className="quiz--button" onClick={handleSubmit} disabled={questions.some(question => question.selected === null)}>
-          Check Answers
+          { hasFinished ? "Play Again" : "Check Answers" }
         </button>
       </div>
     </div>
